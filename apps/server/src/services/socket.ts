f6962a5b4328eb6ec0db169fa2b3a2979e43f876
AdityaGaur7 <adgaur027@gmail.com>
2025-03-02 23:56:40 +0530

    init socket listenners

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -7,6 +7,20 @@ class SocketService {
     console.log("Init Socket Service");
     this._io = new Server();
   }
+
+  public initListeners(){
+    const io = this.io;
+    console.log("Init Socket Listeners");
+    io.on("connection", (socket) => {
+      console.log("New socket connection", socket.id);
+      socket.on("event:message",async ({message}:{message:string}) => {
+        console.log("New Message received", message);
+      });
+    });
+  }
+
+
+
   get io() {
     return this._io;
   }
