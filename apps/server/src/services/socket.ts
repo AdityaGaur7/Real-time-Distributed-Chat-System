484355482ee687422810dcf3dacc60b8117daf97
AdityaGaur7 <adgaur027@gmail.com>
2025-03-03 11:31:58 +0530

    valkey pub-sub done

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,4 +1,18 @@
 import { Server } from "socket.io";
+import Valkey from "iovalkey";
+
+const pub = new Valkey({
+  host: "valkey-338d5f72-adgaur027-82f1.l.aivencloud.com",
+  port: 16015,
+  username: "default",
+  password: "AVNS_jNxe1sE9j4s0y41WanB",
+});
+const sub = new Valkey({
+  host: "valkey-338d5f72-adgaur027-82f1.l.aivencloud.com",
+  port: 16015,
+  username: "default",
+  password: "AVNS_jNxe1sE9j4s0y41WanB",
+});
 
 class SocketService {
   private _io: Server;
@@ -6,25 +20,26 @@ class SocketService {
   constructor() {
     console.log("Init Socket Service");
     this._io = new Server({
-        cors: {
-            allowedHeaders: ["*"],
-            origin: "*",
-        },
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
     });
   }
 
-  public initListeners(){
+  public initListeners() {
     const io = this.io;
     console.log("Init Socket Listeners");
     io.on("connection", (socket) => {
       console.log("New socket connection", socket.id);
-      socket.on("event:message",async ({message}:{message:string}) => {
+      socket.on("event:message", async ({ message }: { message: string }) => {
         console.log("New Message received", message);
+
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
+
       });
     });
   }
-
-
 
   get io() {
     return this._io;
